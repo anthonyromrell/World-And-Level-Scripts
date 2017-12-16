98cdf538561bde23e0d6ddca05e5465be2fa1a6a
Anthony Romrell <anthony@artisandream.com>
2017-12-16 12:08:16 -0700

    Added Singleton

---
+++ Singleton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Singleton {
+
+
+	private static Singleton _instance;
+
+	public static Singleton Instance {
+		get
+		{
+			GetData();
+			return _instance;
+		}
+	}
+
+	private static void GetData () {
+		if (string.IsNullOrEmpty(PlayerPrefs.GetString("GameData")))
+		{
+			_instance = new Singleton();
+		} else
+		{
+			_instance = JsonUtility.FromJson<Singleton>(PlayerPrefs.GetString("GameData"));
+		}
+	}
+
+	public void SaveDataFromInstance () {
+		PlayerPrefs.SetString("GameData", JsonUtility.ToJson(this));
+	}
+
+	public static void SetData () {
+		PlayerPrefs.SetString("GameData", JsonUtility.ToJson(_instance));
+	}
+}
